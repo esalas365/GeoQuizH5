e970b9667901b6e5419d3698388c78d2b61e4bd8
esalas <edsalas365@gmail.com>
2024-03-23 13:51:32 -0500

    CH7 Initial

---
+++ app/src/test/java/com/salas/msu/geoquiz/QuizViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.salas.msu.geoquiz
+
+import androidx.lifecycle.SavedStateHandle
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+internal class QuizViewModelTest {
+    @Test
+    fun providesExpectedQuestionText() {
+        val savedStateHandle = SavedStateHandle()
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
+    }
+
+    @Test
+    fun wrapsAroundQuestionBank() {
+        val savedStateHandle = SavedStateHandle(mapOf(CURRENT_INDEX_KEY to 5))
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        assertEquals(R.string.question_asia, quizViewModel.currentQuestionText)
+        quizViewModel.moveToNext()
+    }
+
+}
